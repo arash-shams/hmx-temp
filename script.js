6aae23e0d58dbc2c9cb3ed12f80b62dd87e42004
arash-shams <122540206+arash-shams@users.noreply.github.com>
2025-12-04 12:34:39 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const progress = document.querySelector(".progress");
+  progress.style.animation = "none";
+  progress.offsetHeight;
+  progress.style.animation = "load 4s infinite";
+});
